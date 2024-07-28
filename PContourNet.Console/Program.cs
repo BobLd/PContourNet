56255225aca740795ca2156c1ab3e5b6edd8ac56
BobLd <38405645+BobLd@users.noreply.github.com>
2024-07-28 09:43:03 +0100

    Update README.md and tidy up code

--- PContourNet.Console/Program.cs
+++ PContourNet.Console/Program.cs
@@ -4,28 +4,28 @@
     {
         static void Main(string[] args)
         {
-
-            int[] bitmap = new int[]{
-                0,0,0,0,0,0,0,0,
-                0,0,0,0,1,1,0,0,
-                0,1,1,1,1,1,0,0,
-                0,1,1,0,0,1,1,0,
-                0,1,1,0,0,1,1,0,
-                0,1,1,1,1,1,0,0,
-                0,0,0,1,1,0,0,0,
-                0,0,0,0,0,0,0,0,
+            int[] bitmap = new int[]
+            {
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 1, 1, 0, 0,
+                0, 1, 1, 1, 1, 1, 0, 0,
+                0, 1, 1, 0, 0, 1, 1, 0,
+                0, 1, 1, 0, 0, 1, 1, 0,
+                0, 1, 1, 1, 1, 1, 0, 0,
+                0, 0, 0, 1, 1, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
             };
 
             int width = 8;
             int height = 8;
 
             // find contours
-            List<PContour.Contour> contours = new PContour().findContours(bitmap, width, height);
+            List<PContour.Contour> contours = PContour.FindContours(bitmap, width, height);
 
             // simplify the polyline
             for (int i = 0; i < contours.Count; i++)
             {
-                contours[i].points = new PContour().approxPolyDP(contours[i].points, 1);
+                contours[i].points = PContour.ApproxPolyDP(contours[i].points, 1);
             }
         }
     }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # PContourNet
+Ported from java to C# from https://github.com/LingDong-/PContour
+
+### From original README:
+Similar to OpenCV's `cv::findContours` and `cv::approxPolyDP`, the algorithms implement the following papers:
+- Suzuki, S. and Abe, K., Topological Structural Analysis of Digitized Binary Images by Border Following
+- David Douglas and Thomas Peucker, Algorithms for the reduction of the number of points required to represent a digitized line or its caricature
